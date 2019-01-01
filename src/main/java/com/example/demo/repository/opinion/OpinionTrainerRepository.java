fa2ef34282c25d5c8e990439579004483cfd3ce7
k96wo <k96.wowczuk@gmail.com>
2019-01-01 22:27:03 +0100

    Commit

--- src/main/java/com/example/demo/repository/opinion/OpinionTrainerRepository.java
+++ src/main/java/com/example/demo/repository/opinion/OpinionTrainerRepository.java
@@ -1,15 +1,15 @@
-package com.example.demo.repository.opinion;
-
-
-import com.example.demo.model.opinion.OpinionTrainer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-
-@Repository
-public interface OpinionTrainerRepository extends JpaRepository<OpinionTrainer,Integer> {
-    List<OpinionTrainer> findByIdOpnionTrainer(int id);
-
-}
+//package com.example.demo.repository.opinion;
+//
+//
+//import com.example.demo.model.opinion.OpinionTrainer;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//
+//@Repository
+//public interface OpinionTrainerRepository extends JpaRepository<OpinionTrainer,Integer> {
+//    List<OpinionTrainer> findByIdOpnionTrainer(int id);
+//
+//}
