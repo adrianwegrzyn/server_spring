c3437c47a13896ad5f8119724c43016fccaa64c8
k96wo <k96.wowczuk@gmail.com>
2019-01-01 22:33:36 +0100

    Commit

--- src/main/java/com/example/demo/repository/exercises/ExercisesOnSevenDaysRepository.java
+++ src/main/java/com/example/demo/repository/exercises/ExercisesOnSevenDaysRepository.java
@@ -1,1 +1,15 @@
-//
+package com.example.demo.repository.exercises;
+
+import com.example.demo.model.exercises.ExercisesOnOneDay;
+import com.example.demo.model.exercises.ExercisesOnSevenDays;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Repository
+public interface ExercisesOnSevenDaysRepository extends JpaRepository<ExercisesOnSevenDays,Integer> {
+
+}
