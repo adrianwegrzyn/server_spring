e0718ed9e0e2430b94a9566117325b1ff69fa0dd
k96wo <k96.wowczuk@gmail.com>
2019-01-01 22:20:58 +0100

    Commit

--- src/main/java/com/example/demo/model/opinion/OpinionDiet.java
+++ src/main/java/com/example/demo/model/opinion/OpinionDiet.java
@@ -1,67 +1,67 @@
-//package com.example.demo.model.opinion;
-//
-//
-//import org.springframework.format.annotation.DateTimeFormat;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.Email;
-//import java.math.BigInteger;
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//
-//
-//@Entity
-//@Table(name = "opinion_diet",schema = "public")
-//public class OpinionDiet {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id_opinion_diet", updatable  =  false , nullable  =  false)
-//    private Integer idOpnionDiet;
-//
-//    @Column(name = "id_user")
-//    private Integer idUser;
-//
-//
-//    @Column(name = "name")
-//    private String name;
-//
-//    @Temporal(TemporalType.DATE)
-//    @Column(name = "date")
-//    private Date date ;
-//
-//    @Column(name = "message")
-//    private String message;
-//
-//    @Column(name = "photo")
-//    private String photo;
-//
-//    public Integer getIdUser() {
-//        return idUser;
-//    }
-//
-//    public String getPhoto() {
-//        return photo;
-//    }
-//
-//    public OpinionDiet(){
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public Date getDate() {
-//        return date;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//    public Integer getIdOpnionDiet() {
-//        return idOpnionDiet;
-//    }
-//
-//
-//}
+package com.example.demo.model.opinion;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "opinion_diet",schema = "public")
+public class OpinionDiet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_opinion_diet", updatable  =  false , nullable  =  false)
+    private Integer idOpnionDiet;
+
+    @Column(name = "id_user")
+    private Integer idUser;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date")
+    private Date date ;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "photo")
+    private String photo;
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public OpinionDiet(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public Integer getIdOpnionDiet() {
+        return idOpnionDiet;
+    }
+
+
+}
