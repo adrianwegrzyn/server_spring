946e4efb2ae780bd9ad1864fdd34ca087e32d97c
k96wo <k96.wowczuk@gmail.com>
2019-01-02 13:52:39 +0100

    Commit

--- src/main/java/com/example/demo/repository/opinion/OpinionDietRepository.java
+++ src/main/java/com/example/demo/repository/opinion/OpinionDietRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository.opinion;
 
-import com.example.demo.model.opinion.OpinionDiet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+        import com.example.demo.model.opinion.OpinionDiet;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OpinionDietRepository extends  JpaRepository<OpinionDiet,Integer>{
